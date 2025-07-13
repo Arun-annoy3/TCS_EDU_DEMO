47aa53c93124af57a3f78f63242b615af813d118
Arun-annoy3 <67721342+Arun-annoy3@users.noreply.github.com>
2025-07-13 21:43:00 +0530

    Add files via upload

---
+++ src/main/java/com/tcs/TCS_EDUREKA/TcsEdurekaApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.TCS_EDUREKA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcsEdurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcsEdurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcs/TCS_EDUREKA/TcsEdurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.TCS_EDUREKA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcsEdurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
