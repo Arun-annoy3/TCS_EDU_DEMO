4c31f4973ca50f96c726fffb5e326805bff97392
Arun-annoy3 <67721342+Arun-annoy3@users.noreply.github.com>
2025-07-14 08:24:11 +0530

    Update TcsEdurekaApplication.java

--- src/main/java/com/tcs/TCS_EDUREKA/TcsEdurekaApplication.java
+++ src/main/java/com/tcs/TCS_EDUREKA/TcsEdurekaApplication.java
@@ -2,6 +2,8 @@ package com.tcs.TCS_EDUREKA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class TcsEdurekaApplication {
@@ -10,13 +12,12 @@ public class TcsEdurekaApplication {
 		SpringApplication.run(TcsEdurekaApplication.class, args);
 	}
 
-	 @RestController
-    class GreetingController {
+	@RestController
+	class GreetingController {
 
-        @GetMapping("/greeting")
-        public String greeting() {
-            return "Hello from TCS Edu Demo!";
-        }
-    }
-
+		@GetMapping("/greeting")
+		public String greeting() {
+			return "Hello from TCS Edu Demo!";
+		}
+	}
 }
